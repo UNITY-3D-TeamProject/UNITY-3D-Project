9dc9fbe799deb944443f49ead026e04389a6a0b0
DESKTOP-B6PNU2R\LSH <dltpgns9710@gmail.com>
2026-09-20 20:57:04 +0900

    feat : Attribute, Motor 어댑터를 객체 어댑터로 변경 - 이세훈

--- Assets/_Project/Scripts/Adapter/AttributeToMotorAdapter.cs
+++ Assets/_Project/Scripts/Adapter/AttributeToMotorAdapter.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 
 namespace Scripts.Adapter
 {
-    public class AttributeToMotorAdapter : CharacterMotor
+    public class AttributeToMotorAdapter : MonoBehaviour
     {
         [SerializeField] private AttributeSet attributeSet;
-        [SerializeField] private string valueKey;
+        [SerializeField] private CharacterMotor characterMotor;
+        [SerializeField] private string speedValueKey;
 
-        public string ValueKey => valueKey;
+        public string SpeedValueKey => speedValueKey;
+
+        private void Awake()
+        {
+            if(!attributeSet) attributeSet = GetComponentInParent<AttributeSet>();
+            
+            if (!attributeSet)
+            {
+                Debug.LogError($"[{name}] AttributeSet is not found", this);
+            }
+        }
 
         private void OnEnable()
         {
+            if (!attributeSet || !characterMotor) return;
+            characterMotor.Speed = attributeSet.GetValue(speedValueKey);
+            attributeSet.AddOnAttributeChangedCallback(OnSpeedChanged);
+        }
+
+        private void OnDisable()
+        {
             if (!attributeSet) return;
-            Speed = attributeSet.GetValue(valueKey);
+            attributeSet.RemoveOnAttributeChangedCallback(OnSpeedChanged);
+        }
+        
+        private void OnSpeedChanged(string attributeKey, float newValue, float oldValue)
+        {
+            if (string.Equals(attributeKey, speedValueKey, StringComparison.OrdinalIgnoreCase))
+            {
+                characterMotor.Speed = newValue; 
+            }
         }
     }
 }
